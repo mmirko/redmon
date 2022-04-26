2f6b27e39423936dec243a76e5f0f0014340a3c1
Mirko Mariotti <mirko@mirkomariotti.it>
2022-04-26 17:39:05 +0200

    Old redmonconfig imported

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mmirko/redmon
 
 go 1.18
+
+require github.com/gomodule/redigo v1.8.8
--- redmon.go
+++ redmon.go
@@ -1,1 +1,13 @@
 package redmon
+
+type SensorConfig struct {
+	Name       string   `json:"name"`
+	Source     string   `json:"source"`
+	Key        string   `json:"key"`
+	SensorType string   `json:"type"`
+	Views      []string `json:"views"`
+	Warning    []string `json:"warning"`
+	Alert      []string `json:"alert"`
+	Refresh    uint16   `json:"refresh"`
+	TTL        uint16   `json:"ttl"`
+}
